64f4807210fc2a0c500caaeb36c882679d92f2a2
SRIRAM21-S <sriramsridher21@gmail.com>
2024-11-26 12:12:34 +0530

    Day3 loops

---
+++ Day3/Loops.java
@@ -0,0 +1,29 @@
+package Day3;
+import java.util.Scanner;
+public class Loops {
+public static void main(String[]args) {
+	
+	Scanner sri= new Scanner(System.in);
+	System.out.println("enter the mark");
+	int mark=sri.nextInt();
+	if(mark>=40){
+
+		if(mark>=90){
+			System.out.println("grade a");}
+		else if(mark>=70){
+			System.out.println("grade b");}
+		
+		else if(mark>=50){
+			System.out.println("grade c");}
+		
+		else {
+			System.out.println("fail");}
+		
+		
+		
+	
+	
+	
+	
+	}
+}}
