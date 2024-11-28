4b8cedfe85f760cca7a9aba04cec06061f17c230
SRIRAM21-S <sriramsridher21@gmail.com>
2024-11-28 12:27:43 +0530

    Day5 StringConcept

---
+++ StringConcept.java
@@ -0,0 +1,22 @@
+package Day5;
+
+import java.util.Scanner;
+
+public class StringConcept {
+	public static void main(String[] args) {
+		
+		Scanner scan=new Scanner(System.in);
+		System.out.println("enter your name");
+		String name=scan.nextLine();
+		System.out.println(name);
+		System.out.println(name.length());
+	    System.out.println(name.substring(0,6));
+		System.out.println(name.charAt(7));
+		
+		
+		
+		
+		
+	}
+
+}
